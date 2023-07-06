cc0e677c1df8fc66381156ebbd4b867844d5beb9
ChienVT <79775410+iamchien@users.noreply.github.com>
2023-07-06 08:22:53 +0700

    Update auto_set_openre_toolchain.sh

--- 5_Development_Toolchain/auto_set_openre_toolchain.sh
+++ 5_Development_Toolchain/auto_set_openre_toolchain.sh
@@ -3,18 +3,34 @@
 # install command
 install_command="sudo apt-get install -y"
 
-sudo apt-get remove modemmanager  
+sudo apt-get remove modemmanager
+$install_command lib32ncurses5 libtool libusb-1.0 libftdi-dev python python-serial python-empy libpython2.7:i386
 
-wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/gcc-arm-none-eabi-5_4-2016q2.tar.bz2
-wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/openocd.tar.bz2
-wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/stlink.tar.bz2
+FILE=./gcc-arm-none-eabi-5_4-2016q2
+if [ ! -d "$FILE" ]; then
+    wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/gcc-arm-none-eabi-5_4-2016q2.tar.bz2
+    tar -jxvf gcc-arm-none-eabi-5_4-2016q2.tar.bz2
+    rm gcc-arm-none-eabi-5_4-2016q2.tar.bz2
+fi
 
-$install_command lib32ncurses5 libtool libusb-1.0 libftdi-dev python python-serial python-empy libpython2.7:i386     
-tar -jxvf gcc-arm-none-eabi-5_4-2016q2.tar.bz2
-tar -jxvf openocd.tar.bz2
-tar -jxvf stlink.tar.bz2
+
+FILE=./openocd
+if [ ! -d "$FILE" ]; then
+    wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/openocd.tar.bz2
+    tar -jxvf openocd.tar.bz2
+    rm openocd.tar.bz2
+fi
+
+
+FILE=./stlink
+if [ ! -d "$FILE" ]; then
+    wget http://handsfree-mv.oss-cn-shenzhen.aliyuncs.com/handsfree_download/OpenRE_Development_Toolchain/stlink.tar.bz2   
+    tar -jxvf stlink.tar.bz2
+    rm stlink.tar.bz2
+fi
+
 cd openocd/
-./configure
+./configure --disable-werror
 make clean
 make
 cd ../stlink/
